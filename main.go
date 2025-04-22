4e7dbfe2859dba998d6a9ba08f62fd1a9285926c
Ell534 <elliot94b@gmail.com>
2025-04-22 16:28:35 +0100

    complete CH1L4 newlines, format the output to display line by line not just 8 byte chunks

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
 )
 
 const inputFile = "messages.txt"
@@ -20,17 +21,31 @@ func main() {
 	fmt.Printf("reading data from input file: %s\n", inputFile)
 	fmt.Println("======================================")
 
+	var currentLine string
+
 	for {
 		bytes := make([]byte, 8)
 		n, err := file.Read(bytes)
+
 		if err != nil {
+			if currentLine != "" {
+				fmt.Printf("read: %s\n", currentLine)
+				currentLine = ""
+			}
 			if errors.Is(err, io.EOF) {
 				break
 			}
 			fmt.Printf("error reading file: %s\n", err.Error())
 			break
 		}
-		strToPrint := string(bytes[:n])
-		fmt.Printf("read: %s\n", strToPrint)
+
+		str := string(bytes[:n])
+		parts := strings.Split(str, "\n")
+
+		for i := 0; i < len(parts)-1; i++ {
+			fmt.Printf("read: %s%s\n", currentLine, parts[i])
+			currentLine = ""
+		}
+		currentLine += parts[len(parts)-1]
 	}
 }
