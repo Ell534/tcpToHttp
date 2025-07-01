a52b66884043287979a7202a4d973034e1e9ef94
Ell534 <elliot94b@gmail.com>
2025-07-01 19:51:00 +0100

    complete CH1L3 read messages.txt file and print to stdout 8 byte chunks

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+const inputFilePath = "messages.txt"
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	file, err := os.Open(inputFilePath)
+	if err != nil {
+		log.Fatalf("failed to open file %s due to error: %s\n", inputFilePath, err)
+	}
+	defer file.Close()
+
+	fmt.Printf("Currently reading data from: %s\n", inputFilePath)
+	fmt.Println("=================================")
+
+	for {
+		bytes := make([]byte, 8, 8)
+		n, err := file.Read(bytes)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			fmt.Printf("error reading from file: %s\n", err.Error())
+			break
+		}
+		stringToPrint := string(bytes[:n])
+		fmt.Printf("read: %s\n", stringToPrint)
+	}
 }
