e1358ee597ddf715ecbd5f32ebfbc9f4ef3df4dc
Ell534 <elliot94b@gmail.com>
2025-07-02 13:56:28 +0100

    complete CH1L5 refactor line reading logic into a function using a go routine and a channel

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
 )
 
 const inputFilePath = "messages.txt"
@@ -15,22 +16,48 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to open file %s due to error: %s\n", inputFilePath, err)
 	}
-	defer file.Close()
 
 	fmt.Printf("Currently reading data from: %s\n", inputFilePath)
 	fmt.Println("=================================")
 
-	for {
-		bytes := make([]byte, 8, 8)
-		n, err := file.Read(bytes)
-		if err != nil {
-			if errors.Is(err, io.EOF) {
+	linesChannel := getLinesChannel(file)
+
+	for line := range linesChannel {
+		fmt.Println("read:", line)
+	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	lines := make(chan string)
+	go func() {
+		defer f.Close()
+		defer close(lines)
+
+		var currentLine string
+		for {
+			bytes := make([]byte, 8, 8)
+			n, err := f.Read(bytes)
+			if err != nil {
+				if currentLine != "" {
+					lines <- currentLine
+				}
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				fmt.Printf("error reading from file: %s\n", err.Error())
 				break
 			}
-			fmt.Printf("error reading from file: %s\n", err.Error())
-			break
+
+			stringToPrint := string(bytes[:n])
+			parts := strings.Split(stringToPrint, "\n")
+
+			for i := 0; i < len(parts)-1; i++ {
+				lines <- fmt.Sprintf("%s%s", currentLine, parts[i])
+				currentLine = ""
+			}
+
+			currentLine += parts[len(parts)-1]
 		}
-		stringToPrint := string(bytes[:n])
-		fmt.Printf("read: %s\n", stringToPrint)
-	}
+	}()
+	return lines
 }
